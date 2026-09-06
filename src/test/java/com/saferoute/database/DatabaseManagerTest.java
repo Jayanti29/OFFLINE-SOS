67d717ba16ef130325f4cf6a1120f55193554c3c
jayanti gautam <182088668+Jayanti29@users.noreply.github.com>
2026-09-07 00:44:47 +0530

    build: add src/test/java/com/saferoute/database/DatabaseManagerTest.java through line 11

--- src/test/java/com/saferoute/database/DatabaseManagerTest.java
+++ src/test/java/com/saferoute/database/DatabaseManagerTest.java
@@ -8,3 +8,4 @@ class DatabaseManagerTest {
     @Test void initializesSchemaAndEnablesForeignKeys() throws Exception {
         var path = Files.createTempFile("saferoute", ".db");
         try (var database = new DatabaseManager(path)) {
+            try (var result = database.connection().createStatement().executeQuery("PRAGMA foreign_keys")) {
