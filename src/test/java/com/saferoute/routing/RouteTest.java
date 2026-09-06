6936b38dd1e3da6f592b04a45d0a7051417d567e
jayanti gautam <182088668+Jayanti29@users.noreply.github.com>
2026-09-07 00:44:49 +0530

    build: add src/test/java/com/saferoute/routing/RouteTest.java through line 14

--- src/test/java/com/saferoute/routing/RouteTest.java
+++ src/test/java/com/saferoute/routing/RouteTest.java
@@ -11,3 +11,4 @@ class RouteTest {
     @Test void copiesLocationIdsAndPreservesRouteMetadata() {
         List<Long> locationIds = new ArrayList<>(List.of(1L, 2L));
         Route route = new Route(locationIds, 100, 75, "SAFE");
+        locationIds.add(3L);
