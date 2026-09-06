137647c4259a7563f8a2c0c1c15e61774ee73490
jayanti gautam <182088668+Jayanti29@users.noreply.github.com>
2026-09-07 00:44:47 +0530

    build: add src/main/java/com/saferoute/routing/RouteFinder.java through line 11

--- src/main/java/com/saferoute/routing/RouteFinder.java
+++ src/main/java/com/saferoute/routing/RouteFinder.java
@@ -8,3 +8,4 @@ public final class RouteFinder {
     private record State(long node, double cost) {}
 
     public Route findShortestRoute(Graph graph, long start, long destination) {
+        return find(graph, start, destination, GraphEdge::distanceMeters, "SHORTEST");
