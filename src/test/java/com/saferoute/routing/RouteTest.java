887456652f090de178b1800b3ac2f4701e88fd33
jayanti gautam <182088668+Jayanti29@users.noreply.github.com>
2026-09-07 00:44:49 +0530

    build: add src/test/java/com/saferoute/routing/RouteTest.java through line 13

--- src/test/java/com/saferoute/routing/RouteTest.java
+++ src/test/java/com/saferoute/routing/RouteTest.java
@@ -10,3 +10,4 @@ import static org.junit.jupiter.api.Assertions.*;
 class RouteTest {
     @Test void copiesLocationIdsAndPreservesRouteMetadata() {
         List<Long> locationIds = new ArrayList<>(List.of(1L, 2L));
+        Route route = new Route(locationIds, 100, 75, "SAFE");
