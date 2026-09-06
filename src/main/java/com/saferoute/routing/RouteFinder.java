9d8646064c2e2a0d39309b5e20b414cca2809105
jayanti gautam <182088668+Jayanti29@users.noreply.github.com>
2026-09-07 00:44:50 +0530

    build: add src/main/java/com/saferoute/routing/RouteFinder.java through line 15

--- src/main/java/com/saferoute/routing/RouteFinder.java
+++ src/main/java/com/saferoute/routing/RouteFinder.java
@@ -12,3 +12,4 @@ public final class RouteFinder {
     }
 
     public Route findSafestRoute(Graph graph, long start, long destination) {
+        return find(graph, start, destination, GraphEdge::safetyCost, "SAFEST");
