54ecbfcf3d25f7e855e09d400975bf63836f75eb
jayanti gautam <182088668+Jayanti29@users.noreply.github.com>
2026-09-07 00:44:45 +0530

    build: add src/test/java/com/saferoute/model/ValidationTest.java through line 9

--- src/test/java/com/saferoute/model/ValidationTest.java
+++ src/test/java/com/saferoute/model/ValidationTest.java
@@ -6,3 +6,4 @@ import static org.junit.jupiter.api.Assertions.*;
 class ValidationTest {
     @Test void rejectsInvalidCoordinates() {
         assertThrows(IllegalArgumentException.class, () -> new Location(1, "x", 91, 0));
+        assertThrows(IllegalArgumentException.class, () -> new Location(1, "x", 0, 181));
