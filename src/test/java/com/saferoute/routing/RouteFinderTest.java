034fb2b7597bca9e982e6932cad5925d3af7b37d
jayanti gautam <182088668+Jayanti29@users.noreply.github.com>
2026-09-07 00:44:48 +0530

    build: add src/test/java/com/saferoute/routing/RouteFinderTest.java through line 12

--- src/test/java/com/saferoute/routing/RouteFinderTest.java
+++ src/test/java/com/saferoute/routing/RouteFinderTest.java
@@ -9,3 +9,4 @@ class RouteFinderTest {
     private Graph graph() {
         Graph graph = new Graph();
         graph.addRoad(new Road(1, 1, 2, 100, 80));
+        graph.addRoad(new Road(2, 2, 4, 100, 80));
