c2c8b1e322d8ea83adb91a76baafa10eb828b634
jayanti gautam <182088668+Jayanti29@users.noreply.github.com>
2026-09-07 00:44:44 +0530

    build: add src/main/java/com/saferoute/Main.java through line 8

--- src/main/java/com/saferoute/Main.java
+++ src/main/java/com/saferoute/Main.java
@@ -5,3 +5,4 @@ import com.saferoute.model.Road;
 import com.saferoute.routing.Route;
 import com.saferoute.routing.RouteFinder;
 import javafx.application.Application;
+import javafx.geometry.Insets;
